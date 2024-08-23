4ab2210b9bf4c5af7577fffce942b588db57ca35
AlinaShahmur <ashaturnaya@gmail.com>
2024-08-23 11:29:58 +0300

    Added new models

---
+++ jobsAnalyze/Models/Process/BE/CreateProcessBE.cs
@@ -0,0 +1,8 @@
+﻿namespace jobsAnalyze.Models.Process.BE
+{
+    public class CreateProcessBE
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ jobsAnalyze/Models/Process/CreateProcessRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jobsAnalyze.Models.Process
+{
+    public class CreateProcessRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
