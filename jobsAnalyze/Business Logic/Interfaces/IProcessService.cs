42f5d605b1c9ad93d73df040ef4936adbd530dd6
AlinaShahmur <ashaturnaya@gmail.com>
2024-08-23 11:32:16 +0300

    Added new method to the Process service and to process service interface

--- jobsAnalyze/Business Logic/Interfaces/IProcessService.cs
+++ jobsAnalyze/Business Logic/Interfaces/IProcessService.cs
@@ -1,9 +1,11 @@
 ﻿using jobsAnalyze.Models.DTO;
+using jobsAnalyze.Models.Process.BE;
 
 namespace jobsAnalyze.Business_Logic.Interfaces
 {
     public interface IProcessService
     {
         public Task<List<Process>> GetProcesses(string userId);
+        public Task<bool> CreateProcess(CreateProcessBE newProcess);
     }
 }
--- jobsAnalyze/Business Logic/ProcessService.cs
+++ jobsAnalyze/Business Logic/ProcessService.cs
@@ -1,6 +1,9 @@
-﻿using jobsAnalyze.Business_Logic.Interfaces;
+﻿using AutoMapper;
+using jobsAnalyze.Business_Logic.Interfaces;
 using jobsAnalyze.Database;
 using jobsAnalyze.Models.DTO;
+using jobsAnalyze.Models.Process.BE;
+using jobsAnalyze.ResponseWrapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace jobsAnalyze.Business_Logic
@@ -9,14 +12,31 @@ namespace jobsAnalyze.Business_Logic
     {
 
         private readonly ApplicationDbContext _context;
-        public ProcessService(ApplicationDbContext context) 
+        private readonly IMapper _mapper;
+        public ProcessService(ApplicationDbContext context, IMapper mapper) 
         {
             _context = context;
+            _mapper = mapper;
         }
 
         public async Task<List<Process>> GetProcesses(string userId)
         {
             return await _context.Processes.Where(x => x.UserId == userId).ToListAsync<Process>();
+        }
+
+        public async Task<bool> CreateProcess(CreateProcessBE process)
+        {
+            try
+            {
+                Process processDto = _mapper.Map<Process>(process);
+                await _context.Processes.AddAsync(processDto);
+                int resCount = await _context.SaveChangesAsync();
+                return resCount > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
