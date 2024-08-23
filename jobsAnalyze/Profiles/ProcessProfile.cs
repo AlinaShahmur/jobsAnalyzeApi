147f1c652773bf8315bdb0a9605719106b82e824
AlinaShahmur <ashaturnaya@gmail.com>
2024-08-23 11:32:41 +0300

    Added process profile

---
+++ jobsAnalyze/Profiles/ProcessProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using jobsAnalyze.Models.DTO;
+using jobsAnalyze.Models.Process;
+using jobsAnalyze.Models.Process.BE;
+
+namespace jobsAnalyze.Profiles
+{
+    public class ProcessProfile : Profile
+    {
+        public ProcessProfile() 
+        {
+            CreateMap<CreateProcessRequest, CreateProcessBE>()
+                .ForMember(p => p.UserId, opt => opt.Ignore());
+            CreateMap<CreateProcessBE, Process>();
+        }
+    }
+}
