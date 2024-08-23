293c9250c9c982640257e1037212da03290e4ca0
AlinaShahmur <ashaturnaya@gmail.com>
2024-08-23 11:29:31 +0300

    Added get process and create process actions in process controller

--- jobsAnalyze/Controllers/ProcessController.cs
+++ jobsAnalyze/Controllers/ProcessController.cs
@@ -1,5 +1,8 @@
-﻿using jobsAnalyze.Business_Logic.Interfaces;
+﻿using AutoMapper;
+using jobsAnalyze.Business_Logic.Interfaces;
 using jobsAnalyze.Models.DTO;
+using jobsAnalyze.Models.Process;
+using jobsAnalyze.Models.Process.BE;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,23 +14,52 @@ namespace jobsAnalyze.Controllers
     public class ProcessController : ControllerBase
     {
         private readonly IProcessService _processService;
-        public ProcessController(IProcessService processService) 
+        private readonly IMapper _mapper;
+        public ProcessController(IProcessService processService, IMapper mapper) 
         {
             _processService = processService;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [Authorize]
         public async Task<List<Process>> GetProcesses()
         {
-            var userIdClaim = HttpContext.User.FindFirst("userId");
-            if (userIdClaim != null)
+            string? userId = GetCurrentUserId();
+            if (userId != null)
             {
-                string? userId = userIdClaim.Value;
                 return await _processService.GetProcesses(userId);
             }
             return null;
         }
 
+        [HttpPost]
+        [Authorize]
+
+        public async Task<bool> CreateProcess(CreateProcessRequest newProcess)
+        {
+            string? userId = GetCurrentUserId();
+            if (userId != null)
+            {
+                CreateProcessBE processBE = _mapper.Map<CreateProcessBE>(newProcess, opt =>
+                    {
+                        opt.AfterMap((src, dest) => dest.UserId = userId);
+                    }
+                );
+                return await _processService.CreateProcess(processBE);
+            }
+            return false;
+        }
+
+        private string? GetCurrentUserId()
+        {
+            var userIdClaim = HttpContext.User.FindFirst("userId");
+            if (userIdClaim != null)
+            {
+                string? userId = userIdClaim.Value;
+                return userId;
+            }
+            return null;
+        }
     }
 }
